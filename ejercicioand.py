4f6ef3d7efc8c316443486da18fbccf63616202a
Daniel <romero.g.daniel@gmail.com>
2025-04-24 08:15:44 -0500

    Actividad de prueba

--- ejercicioand.py
+++ ejercicioand.py
@@ -4,7 +4,18 @@
 #si se realiza una compra de 10 relojes se hara un descuento del 10%
 #si se realiza una compra de 20 relojes se hara un descuento del 20%
 
-print("ingresa nombre")
-nombre=input()
+reloj = int(input("ingresar numero de relojes"))
+precio_unitario= int(input("ingresar precio del reloj"))
 
-print("hola "+nombre+"")
+if reloj >= 10:
+    descuento = 0.10
+
+elif reloj >= 20:
+    descuento = 0.20
+
+subtotal= reloj * precio_unitario 
+total= (subtotal*descuento)
+
+print(f"|subtotal|{subtotal}")
+print(f"descuento aplicado: {descuento*100}%")
+print(f"|total a pagar:|{total}")
