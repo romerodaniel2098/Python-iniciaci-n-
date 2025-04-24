c029732583b7dc44d3385bd38ce4a82b94ed0f47
Daniel <romero.g.daniel@gmail.com>
2025-04-24 06:43:41 -0500

    Bienvenido

---
+++ ejercicioand.py
@@ -0,0 +1,7 @@
+
+
+#este programa nos ayudara a aolicar los docuentos por el numero de compras realizadas
+#si se realiza una compra de 10 relojes se hara un descuento del 10%
+#si se realiza una compra de 20 relojes se hara un descuento del 20%
+
+print("")
